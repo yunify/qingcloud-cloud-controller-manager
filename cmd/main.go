b881e88ccf82c750d474efdfc41f193c159d0317
Xuetao Song <magicsong@yunify.com>
2019-10-30 23:17:57 +0800

    remove  check in get loadbalancer (#88)
    * remove  check in get loadbalancer
    
    * fix ci
    
    * fix ci

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,6 @@ func main() {
 	// TODO: once we switch everything over to Cobra commands, we can go back to calling
 	// utilflag.InitFlags() (by removing its pflag.Parse() call). For now, we have to set the
 	// normalize func and add the go flag set by hand.
-	// utilflag.InitFlags()
 
 	logs.InitLogs()
 	defer logs.FlushLogs()
